526e0bcaf0ce2681f1302eb48f0a52416724f871
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-02-18 21:02:52 -0500

    Add files via upload

--- SQL Project 1.sql
+++ SQL Project 1.sql
@@ -20,4 +20,23 @@ FROM
     /* 1. Write a SQL statement to display all the information of all salesmen.  */
 SELECT * FROM salesman;
 
-        
+
+
+/* 1. Write a SQL statement to find those salesmen with all information who come from the city either Paris or Rome. */
+
+SELECT * 
+FROM salesman
+Where city = 'Paris' OR city ='Rome';           
+
+/*2. Write a query to filter those salesmen with all information who comes from any of the cities Paris and Rome.*/  
+
+SELECT *
+FROM salesman
+Where city IN ('Paris', 'Rome');
+
+/* 3. Write a query to produce a list of salesman_id, name, city and commission of each salesman who live in cities other than Paris and Rome.  */
+
+SELECT *
+FROM salesman
+WHERE city NOT IN ('Paris', 'Rome');
+
