9e0f7d84a5169cbd424ce50b3d905eb8aa845b9e
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-02-26 15:05:00 -0500

    Update Data Science Independent Project #1 – Watching the Stock Market.sql

--- Data Science Independent Project #1 – Watching the Stock Market/Data Science Independent Project #1 – Watching the Stock Market.sql
+++ Data Science Independent Project #1 – Watching the Stock Market/Data Science Independent Project #1 – Watching the Stock Market.sql
@@ -7,7 +7,8 @@ SELECT * FROM stocks
 WHERE symbol = 'GM';
 /* The stock has been on a slight downward trend from rougly $56 at the start of the period, to $51 at the end. 
 GM announced its Q4 2020 earnings on February 10th, 2021 and easily beat Wall Street expectations, however, despite this better-than-expected performance, stock price fell immediately following the announcement.
-This was because of the warning issued by the company that 2021 earnings could fall by $1.5 Billion to $2 Billion on the back of a global semiconductor chip shortage that will affect production. Since stock prices reflect investors sentiment about the FUTURE performance of a company, this fall in share price before actual earnings are announced for 2021 were to be expected. */
+This was because of the warning issued by the company that 2021 earnings could fall by $1.5 Billion to $2 Billion on the back of a global semiconductor chip shortage that will affect production. 
+Since stock prices reflect investors sentiment about the FUTURE performance of a company, this fall in share price before actual earnings are announced for 2021 were to be expected. */
 
 /* Which rows have a price above 100? */
 SELECT * FROM stocks
@@ -28,4 +29,33 @@ ORDER BY price DESC;
 SELECT name, MIN(price) FROM stocks;
 
 SELECT name, MAX(price) FROM stocks;
+
+
+                                                                          	/* Intermediate Challenge */
+
+/* Explore using aggregate functions to look at key statistics about the data (e.g., min, max, average). */			
+
+SELECT AVG(price) FROM stocks;		
+    -- The average of all prices is $93.88. -- 
+    
+SELECT COUNT(price) FROM stocks;					
+    -- There are 150 entries in the 'stocks' table. -- 
+
+SELECT SUM(price) FROM stocks;
+
+
+/* Group the data by stock and repeat. How do the stocks compare to each other? */
+
+SELECT name, AVG (price) FROM stocks GROUP BY name;
+SELECT name, COUNT(price) FROM stocks GROUP BY name;
+SELECT name, SUM(price) FROM stocks GROUP BY name;
+SELECT name, MAX(price) FROM stocks GROUP BY name;
+SELECT name, MIN(price) FROM stocks GROUP BY name;
+
+
+/* Group the data by day or hour of day. Does day of week or time of day impact prices? */
+
+-- at closing: --
+SELECT name, price FROM stocks WHERE datetime LIKE '% 16:00:00' GROUP BY name; 
+
 
