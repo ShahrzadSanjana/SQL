7104ce51993dbc6d7123f74e99dfcef1e6fd9bc9
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-03-09 00:28:00 -0500

    Update Data Science Independent Project #1 – Watching the Stock Market.sql

--- Data Science Independent Project #1 – Watching the Stock Market/Data Science Independent Project #1 – Watching the Stock Market.sql
+++ Data Science Independent Project #1 – Watching the Stock Market/Data Science Independent Project #1 – Watching the Stock Market.sql
@@ -8,12 +8,12 @@ WHERE symbol = 'GM';
 /* The stock has been on a slight downward trend from rougly $56 at the start of the period, to $51 at the end. 
 GM announced its Q4 2020 earnings on February 10th, 2021 and easily beat Wall Street expectations, however, despite this better-than-expected performance, stock price fell immediately following the announcement.
 This was because of the warning issued by the company that 2021 earnings could fall by $1.5 Billion to $2 Billion on the back of a global semiconductor chip shortage that will affect production. 
-Since stock prices reflect investors sentiment about the FUTURE performance of a company, this fall in share price before actual earnings are announced for 2021 were to be expected. */
+Since stock prices reflect investors sentiment about the FUTURE performance of a company, this fall in share price before actual earnings are announced for 2021 was to be expected. */
 
 /* Which rows have a price above 100? */
 SELECT * FROM stocks
 WHERE price > '100';
--- Microsoft's stock has a price that has been well above the $100 mark throughout the entire period. --
+-- Microsoft's stock price has been well above the $100 mark throughout the entire period. --
 
 /* between 40 to 50, etc? */
 SELECT * FROM stocks
