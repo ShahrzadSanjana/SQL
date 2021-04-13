60b314c6a6d68343d37b00d5512f82f8e572f2c6
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-04-12 21:09:17 -0400

    Update and rename Select within Select statements to Select within Select statements.sql

--- Select within Select statements.sql
+++ Select within Select statements.sql
@@ -31,8 +31,11 @@ FROM world
 WHERE population < (SELECT population FROM world WHERE name = 'Poland') AND population > (SELECT population FROM world WHERE name = 'Canada');
 
 
-/* 5) */
+/* 5) Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany. */
 
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany'),0),'%')
+FROM world
+WHERE continent = 'Europe';
 
 
 /* 6) Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values) */
