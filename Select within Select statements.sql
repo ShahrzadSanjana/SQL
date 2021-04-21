89882c65ee19a8dfee9de65096ff8d0e6f71bc4e
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-04-21 10:40:27 -0400

    Update Select within Select statements.sql

--- Select within Select statements.sql
+++ Select within Select statements.sql
@@ -65,3 +65,19 @@ FROM world
 WHERE continent IN (SELECT continent FROM world WHERE name = 'Brazil');
 
 
+/* 2) List each country and its continent in the same continent as 'Brazil' or 'Mexico'. */
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name = 'Brazil' OR name = 'Mexico');
+
+
+/* 3) Show the population of China as a multiple of the population of the United Kingdom */
+
+
+
+/* 4) Show each country that has a population greater than the population of ALL countries in Europe. Note that we mean greater than every single country in Europe; not the combined population of Europe.*/
+
+SELECT name, population
+FROM world 
+WHERE population > ALL (SELECT population FROM world WHERE continent = 'Europe');
