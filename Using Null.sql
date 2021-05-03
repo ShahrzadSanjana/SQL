ed9f84839a36d62ae683e83db7ac2cce606cba1b
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-05-02 20:49:37 -0400

    Update Using Null.sql

--- Using Null.sql
+++ Using Null.sql
@@ -26,4 +26,14 @@ WHERE teacher.name IS NOT NULL;
 
 /* 4) Use a different JOIN so that all departments are listed. */
 
+SELECT teacher.name, dept.name
+FROM teacher
+RIGHT JOIN dept ON teacher.dept=dept.id
+WHERE dept.name IS NOT NULL;
 
+
+/* 5) Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266' */
+
+
+
+ 
