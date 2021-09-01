200fec30fb8f532807a7b178305805788ceeddaa
ShahrzadSanjana <78444311+ShahrzadSanjana@users.noreply.github.com>
2021-09-01 18:52:55 -0400

    Update Using Null.sql

--- Using Null.sql
+++ Using Null.sql
@@ -38,8 +38,12 @@ SELECT name, COALESCE (mobile, '07986 444 2266')
 FROM teacher;
 
 
-/* 6) */
+/* 6) Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department. */
 
+SELECT teacher.name, COALESCE (dept.name, 'None') AS dept_name
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept=dept.id;
 
 
 /* 7) Use COUNT to show the number of teachers and the number of mobile phones. */
